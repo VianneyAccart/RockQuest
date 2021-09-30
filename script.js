c20a350fd488bac416c8e27846df89221a0dc7c3
Meziani Ismaël <81325578+BeerusHakaishin@users.noreply.github.com>
2021-09-30 10:02:11 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//Countdown 60 to 0 sec
+
+let timeleft = 60;
+let downloadTimer = setInterval(function(){
+    document.getElementById("progressBar").value = 60 - timeleft;
+    timeleft-- ;
+    document.getElementById("countdownText").innerHTML = timeleft;
+    if(timeleft <= 0){clearInterval(downloadTimer);}
+   							 	},1000);  
