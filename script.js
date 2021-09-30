6861e0d507adb4f31e17037bbf5d71bc197145c1
Meziani Ismaël <81325578+BeerusHakaishin@users.noreply.github.com>
2021-09-30 10:03:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Countdown 60 to 0 sec
+// Countdown 60 to 0 sec
 
 let timeleft = 60;
 let downloadTimer = setInterval(function(){
@@ -7,3 +7,39 @@ let downloadTimer = setInterval(function(){
     document.getElementById("countdownText").innerHTML = timeleft;
     if(timeleft <= 0){clearInterval(downloadTimer);}
    							 	},1000);  
+
+// Countdown start new game and show content 
+
+let timeout,interval;
+let startNewPlay = 3000;
+let secondsleft= startNewPlay;
+
+ function showContent()
+ {
+    secondsleft-=1000;
+    document.querySelector(".div1").innerHTML = "New game start in " + Math.abs((secondsleft/1000))+" secs";  
+    
+    if(secondsleft == 0)
+    {
+        //document.getElementById("clickme").style.display="";
+        clearInterval(interval);
+        document.querySelector(".div1").style.display="none";
+        document.querySelector(".div2").style.display="";
+    }
+};
+
+function startCountdown()
+{
+      clearInterval(interval);
+      secondsleft=startNewPlay;
+      document.querySelector(".div1").innerHTML = "New game start in " + Math.abs((secondsleft/1000))+" secs";  
+       interval= setInterval(function()
+       {
+           showContent();
+       },1000)              
+};
+
+/* function resetTimer()
+{
+    startCountdown();
+}; */
