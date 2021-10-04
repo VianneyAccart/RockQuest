5e34ff022d64ec6f2c4a8c70591d00ac6c92cd74
Vianney Accart <vianneyaccart@gmail.com>
2021-10-04 19:26:43 +0200

    removing notes after 2 sec

--- js/script.js
+++ js/script.js
@@ -42,10 +42,10 @@ function addNote(noteId, color) {
 
 // Partitions
 const easyMode = {
-    S: [59, 55, 52, 50, 45, 30, 20, 10],
-    D: [57, 49, 39, 29, 19, 9],
-    K: [50, 51, 46, 41, 35, 8],
-    L: [48, 44, 31, 28, 22, 5]
+    S: [59, 55, 52, 50, 46, 42, 37, 34, 30, 26, 22, 20, 16, 10, 1],
+    D: [57, 49, 45, 43, 39, 34, 32, 29, 19, 9],
+    K: [55, 53, 51, 47, 48, 46, 41, 35, 32, 28, 22, 17, 14, 11, 8],
+    L: [58, 54, 48, 44, 31, 28, 22, 5]
 };
 
 // Countdown start new game and show content 
@@ -91,7 +91,8 @@ function progressBarCount() {
             clearInterval(downloadTimer); 
             document.querySelector(".end-game-popup").style.display = "flex"; 
         }
-        // Génère les notes selon la partition
+        // Génère les notes de la partition Easy
+            // Créer une nouvelle fonction pour le D.R.Y
         for (let i = 0; i < easyMode.S.length; i++) {
             if (easyMode.S[i] === timeleft) {
                 addNote("noteS", "green");
@@ -114,3 +115,20 @@ function progressBarCount() {
         }
     }, 1000);
 }
+
+// Cible la note et les boutons
+let myNote = document.querySelectorAll(".note");
+
+// Supprime la note après 2 secondes
+
+setTimeout(function() { 
+    for (let i = 0; i < myNote.length; i++) {
+        myNote[i].style.display = "none";
+    }
+}, 2000);
+
+/* PISTES A EXPLORER
+* Quand ma note touche le bas de la lettre, elle se supprime.
+* DOM.top : position du top + hauteur de l'élément = position bottom
+* Faire gaffe, si on appuie trop tot, la note est supprimée => je supprime la note la plus basse.
+*/
